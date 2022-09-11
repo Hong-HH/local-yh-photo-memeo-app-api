d3d65b3e522195192fc3a4fb3f8c268f1c572465
Hong-HH <a1guskn7@gmail.com>
2022-09-11 18:29:50 +0900

    logout_and_RevokedTokenError

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask.json import jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from flask_jwt_extended.exceptions import RevokedTokenError
 
 from http import HTTPStatus
 
@@ -9,6 +10,10 @@ from config import Config
 
 # resource 클래스 import
 from resources.register import UserRegisterResource
+from resources.logout import LogoutResource
+from resources.blocklist import check_blocklist
+
+
 
 
 
@@ -20,31 +25,53 @@ app.config.from_object(Config)
 # JWT 토큰 만들기
 jwt = JWTManager(app)
 
-# 로그아웃한 유저의 jti를 확인하는 코드 
-# todo 로그아웃 관련 리소스 생성 후 주석 해제
-# @jwt.token_in_blocklist_loader
-# def check_if_token_is_revoked(jwt_header, jwt_payload) :
-#     # jti = jwt_payload['jti']
-#     # return jti in jwt_blacklist
-#     jti = jwt_payload['jti']
-#     result = check_blocklist(jti)
+# 로그아웃한 유저인지 확인하는 코드 
+@jwt.token_in_blocklist_loader
+def check_if_token_is_revoked(jwt_header, jwt_payload) :
+    print("Function check_if_token_is_revoked is activated")
+    jti = jwt_payload['jti']
+    result = check_blocklist(jti)
+    # True 일때 이미 로그아웃한 유저, False 일때 로그아웃 아직 안한 유저
+    print("check_blocklist 결과는 {} 입니다.".format(str(result)))
     
-#     return result
+    return result
 
 
-api = Api(app)
+
+# 만료된 토큰일 경우의 반환값
+# 테스트 필요 : 현재는 토큰 만료가 안되게 설정해서 ... 
+# @jwt.expired_token_loader
+# def expire_token_callback (jwt_header, jwt_payload):
+#     print("Function expire_token_callback is activated")
+
+#     return {'result':'해당 토큰은 만료되었습니다.'}
+
+# catch 하고싶은 에러 
+CUSTOM_ERRORS = {
+'RevokedTokenError': {
+        'message': "이미 로그아웃 되었습니다(401 Unauthorized).", 'status' : 401
+    },
+}
+
+
+api = Api(app, errors=CUSTOM_ERRORS)
+
+
+
+
+
 
 # resources 와 연결 
 # todo 리소스 생성완료시 하나씩 주석 해재해서 테스트하기
 
 api.add_resource(UserRegisterResource, '/v1/user/register')
 # api.add_resource(UserLoginResource, '/v1/user/login')
-# api.add_resource(LogoutResource, '/v1/user/logout')
+api.add_resource(LogoutResource, '/v1/user/logout')
 # api.add_resource(MemoListResource, '/v1/memo')
 # api.add_resource(MemoResource, '/v1/memo/<int:memo_id>')
 
 
-
+# 연결확인용 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
